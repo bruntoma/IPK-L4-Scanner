a8910d7bedd8bba9aabb560b54d14af389a7d0a0
bruntoma <bruntom@seznam.cz>
2025-03-20 15:21:22 +0100

    refactor: Split Scanner into multiple classes

---
+++ IPK-L4-Scanner.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace IPK_L4_Scanner.Tests;
+using Xunit;
+using System.Net;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    { 
+        Console.WriteLine("asdf");
+    }
+}
