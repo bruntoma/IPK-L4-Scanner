788fe341e9eb1bb974702d2ae35b7dad0ba88695
Tomáš Brůna <bruntoma@seznam.cz>
2026-03-31 08:08:05 +0200

    Remove unused files

--- PacketFactory.cs
+++
@@ -1,9 +0,0 @@
-namespace IPK_L4_Scanner;
-
-class PacketFactory 
-{
-    public byte[] CreatePacket(ScannerProtocol protocolType)
-    {
-        return null;
-    }
-}
--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IPK_L4_Scanner;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
